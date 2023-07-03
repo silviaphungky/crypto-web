23d77013e7f46af62eeeb02c07944dc7b0c273a8
silviaphungky <silvia.phungky@mandirisek.co.id>
2023-07-03 23:48:31 +0700

    custom app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,32 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@styles/globals.css'
+import type { AppContext, AppProps } from 'next/app'
+import { ReactElement, ReactNode } from 'react'
+import App from 'next/app'
+import { NextPage } from 'next'
+import Providers from '@utils/provider'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode
 }
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout
+}
+
+const Container = ({ Component, pageProps }: AppPropsWithLayout) => {
+  const getLayout = Component.getLayout ?? ((page) => page)
+
+  return <Providers>{getLayout(<Component {...pageProps} />)}</Providers>
+}
+
+function MyApp({ ...props }: AppProps) {
+  return <Container {...props} />
+}
+
+MyApp.getInitialProps = async (appContext: AppContext) => {
+  const appProps = await App.getInitialProps(appContext)
+
+  return { ...appProps }
+}
+
+export default MyApp
