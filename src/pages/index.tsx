1e440a22267def6bcd16ebda0116458851cd9e68
silviaphungky <silvia.phungky@mandirisek.co.id>
2023-07-03 23:51:05 +0700

    redirect to market page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,17 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { PATHS } from '@constants/paths'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const router = useRouter()
+  useEffect(() => {
+    router.push(PATHS.MARKET)
+  }, [])
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
