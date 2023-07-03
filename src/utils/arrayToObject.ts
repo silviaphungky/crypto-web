62d96390d498c11e2b924c7a52b40de41f997a35
silviaphungky <silvia.phungky@mandirisek.co.id>
2023-07-03 23:52:21 +0700

    add reusable constant and utils

---
+++ src/utils/arrayToObject.ts
@@ -0,0 +1,10 @@
+const arrayToObject = <T extends Record<K, any>, K extends keyof any>(
+  array: T[] = [],
+  getKey: (item: T) => K
+) =>
+  array.reduce((obj, cur) => {
+    const key = getKey(cur)
+    return { ...obj, [key]: cur }
+  }, {} as Record<K, T>)
+
+export default arrayToObject
