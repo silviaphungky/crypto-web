4ca70e75eb357834376295e24865bf9a13f9ca94
silviaphungky <silvia.phungky@mandirisek.co.id>
2023-07-03 23:45:24 +0700

    set tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,32 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/domains/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      borderWidth: {
+        1: '1px',
+      },
+      colors: {
+        bullish: '#25a764',
+        bearish: '#e54040',
+        'gray-400': '#929396',
+      },
+      backgroundColor: {
+        'gray-200': '#f2f2f2',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+    },
+    screens: {
+      sm: { max: '767px' },
+      md: { min: '768px', max: '1023px' },
+      lg: { min: '1024px', max: '1279px' },
+      xl: { min: '1280px' },
     },
   },
   plugins: [],
