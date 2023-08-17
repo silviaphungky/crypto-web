5043442263591609679cbce7456b6dbb8ae28d9f
silviaphungky <silvia.phungky@mandirisek.co.id>
2023-08-17 21:39:28 +0700

    create roundshimmer component

---
+++ src/components/RoundShimmer/RoundShimmer.tsx
@@ -0,0 +1,8 @@
+const RoundShimmer = () => {
+  return (
+    <div className="animate-pulse flex">
+      <div className="rounded-full bg-gray-300 h-9 w-9"></div>
+    </div>
+  )
+}
+export default RoundShimmer
