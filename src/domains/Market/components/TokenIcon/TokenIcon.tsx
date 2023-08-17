9a02e1e3fe92565b4358e3b6b7d0dcfff4625c0e
silviaphungky <silvia.phungky@mandirisek.co.id>
2023-08-17 22:17:43 +0700

    console error

--- src/domains/Market/components/TokenIcon/TokenIcon.tsx
+++ src/domains/Market/components/TokenIcon/TokenIcon.tsx
@@ -1,13 +1,14 @@
-import styles from './_TokenIcon.module.css'
 import { memo } from 'react'
 import Image from 'next/image'
-import { Shimmer } from '@components'
+import { RoundShimmer } from '@components'
+
+import styles from './_TokenIcon.module.css'
 
 const TokenIcon = ({ url, alt }: { url: string; alt: string }) => {
   return url ? (
     <Image className={styles.icon} src={url} alt={alt} width={35} height={35} />
   ) : (
-    <Shimmer />
+    <RoundShimmer />
   )
 }
 
--- src/pages/api/icon.ts
+++ src/pages/api/icon.ts
@@ -22,7 +22,7 @@ export default async function handler(
     const data = await response.json()
     res.status(200).json(data)
   } catch (error) {
-    console.error(error)
+    console.error(error.response)
     res.status(500).json({ message: 'Error fetching data' })
   }
 }
