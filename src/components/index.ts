ae3466d93bb45bc7251edce86f7418fc4c3bf5a9
silviaphungky <silvia.phungky@mandirisek.co.id>
2023-08-17 23:13:12 +0700

    fix ts error

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,6 +3,6 @@ import CandleStick from './Candlestick'
 import Orderbook from './Orderbook'
 import Shimmer from './Shimmer'
 import RoundShimmer from './RoundShimmer'
-import RunningTrade from './RunningTrade'
+import { RunningTrade } from './RunningTrade'
 
 export { Select, CandleStick, Orderbook, Shimmer, RoundShimmer, RunningTrade }
